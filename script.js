254858b2177909cda03baf9e3664c25e76043cdd
ieufafal <falahalyprabowo@gmail.com>
2024-11-10 13:58:26 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function showLove() {
+ 
+    alert('Haloo cantik, mabruk Alfa mabruk yaa icil kesayangan bacill🤗Semua yg sudah berlalu dilupakan yaa semoga diusia kamu yang sekarang semakin dipermudah urusannya dan diberikan rezeki yg berlimpah dari Allah SWT, for this year semoga kamu semakin diperluas rasa kesabarannya semakin semangat untuk menjalani hidup dan bisa mewujudkan semua impian kamu di masa yg akan datang,aku berdoa selalu untuk kesehatan kamu dan dijauhkan dari segala hal hal tidak baik dari segi pertemanan ataupun pergaulan yaa sayangg🫶🏻. Wish you all the best dari aku untuk kamu tunggu yaa nnti aku datang dan kita akan menata hubungan untuk kejenjang yg lebih serius amin ya rabbal alamin. I love you more ❤️');
+ 
+}
